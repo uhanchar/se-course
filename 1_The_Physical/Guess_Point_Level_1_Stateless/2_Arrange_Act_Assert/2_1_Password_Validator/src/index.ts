517484ab5479dac5b5da4c118594299d5b14f8b6
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-18 23:50:45 +0200

    [Refactor]: Move password length check and digit check into separate methods

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,8 +1,16 @@
 export class PasswordValidator {
   public validate(password: string) {
-    const isCorrectLength = password.length > 5 && password.length < 16;
-    const hasDigit = /\d/.test(password);
+    const isCorrectLength = this.validateLength(password);
+    const hasDigit = this.validateDigit(password);
 
     return isCorrectLength && hasDigit;
+  }
+
+  private validateLength(password: string): boolean {
+    return password.length > 5 && password.length < 16;
+  }
+
+  private validateDigit(password: string): boolean {
+    return /\d/.test(password);
   }
 }
