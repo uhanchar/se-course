ed4abdda3880dbd7ffa04e4de81d27f453864e46
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-18 23:56:04 +0200

    [Refactor]: Move sut creation to beforeEach

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,11 +1,15 @@
 import { PasswordValidator } from './index';
 
 describe('PasswordValidator tests', () => {
+  let sut: PasswordValidator;
+
+  beforeEach(() => {
+    sut = new PasswordValidator();
+  });
+
   describe('when password correct', () => {
     describe('when password is between 5 and 15 characters long', () => {
       it('should return true', () => {
-        const sut = new PasswordValidator();
-
         const result = sut.validate('testPass1');
 
         expect(result).toBeTruthy();
@@ -14,8 +18,6 @@ describe('PasswordValidator tests', () => {
 
     describe('when password is has at least 1 digit', () => {
       it('should return true', () => {
-        const sut = new PasswordValidator();
-
         const result = sut.validate('testPass2');
 
         expect(result).toBeTruthy();
@@ -24,8 +26,6 @@ describe('PasswordValidator tests', () => {
 
     describe('when password is has at least uppercase letter', () => {
       it('should return true', () => {
-        const sut = new PasswordValidator();
-
         const result = sut.validate('testPass3');
 
         expect(result).toBeTruthy();
