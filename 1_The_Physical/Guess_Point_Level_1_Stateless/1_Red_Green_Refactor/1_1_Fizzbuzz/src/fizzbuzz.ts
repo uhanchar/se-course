45b85a7039bebc59be02ba2c9755477cfab2913a
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-18 23:25:35 +0200

    [New]: ensure fizzBuzz throws an error if input is less than 1

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,8 @@
 export function fizzBuzz(input: number): string | number {
+  if (input < 1) {
+    throw new Error('Input is less than 1');
+  }
+
   if (input % 3 === 0 && input % 5 === 0) {
     return 'FizzBuzz';
   } else if (input % 3 === 0) {
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,27 +1,37 @@
 import { fizzBuzz } from './fizzbuzz';
 
 describe('fizzbuzz tests', () => {
-  describe('when number is multiple of 3', () => {
-    it("should return 'Fizz'", () => {
-      expect(fizzBuzz(6)).toBe('Fizz');
+  describe('when number is not valid', () => {
+    describe('when number is less than 1', () => {
+      it("should throw an error", () => {
+        expect(() => fizzBuzz(-1)).toThrow('Input is less than 1');
+      });
     });
   });
 
-  describe('when number is multiple of 5', () => {
-    it("should return 'Buzz'", () => {
-      expect(fizzBuzz(25)).toBe('Buzz');
+  describe('when number is valid', () => {
+    describe('when number is multiple of 3', () => {
+      it("should return 'Fizz'", () => {
+        expect(fizzBuzz(6)).toBe('Fizz');
+      });
     });
-  });
 
-  describe('when number is multiple of 3 and 5', () => {
-    it("should return 'FizzBuzz'", () => {
-      expect(fizzBuzz(15)).toBe('FizzBuzz');
+    describe('when number is multiple of 5', () => {
+      it("should return 'Buzz'", () => {
+        expect(fizzBuzz(25)).toBe('Buzz');
+      });
     });
-  });
 
-  describe('when number is not multiple of 3 or 5', () => {
-    it('should return number', () => {
-      expect(fizzBuzz(16)).toBe('16');
+    describe('when number is multiple of 3 and 5', () => {
+      it("should return 'FizzBuzz'", () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+      });
+    });
+
+    describe('when number is not multiple of 3 or 5', () => {
+      it('should return number', () => {
+        expect(fizzBuzz(16)).toBe('16');
+      });
     });
   });
 });
