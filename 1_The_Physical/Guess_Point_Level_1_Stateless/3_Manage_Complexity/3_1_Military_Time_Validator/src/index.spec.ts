34a5fce0e1cfa4633076b415ed2dbcc4bf8810db
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-19 19:53:25 +0200

    [New]: Ensure military time returns true if correct input

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,10 +2,16 @@ import { isMilitaryTime } from './index';
 
 describe('militaryTime validator tests', () => {
   describe('when incorrect error', () => {
-    describe('when not correct input', () => {
+    describe('when input not correct', () => {
       it('should return false', () => {
         expect(isMilitaryTime('')).toBeFalsy();
       });
+    });
+  });
+
+  describe('when input correct', () => {
+    it('should return true', () => {
+      expect(isMilitaryTime('04:23 - 14:45')).toBeTruthy();
     });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,19 @@
 export const isMilitaryTime = (input: string): boolean => {
-  return false;
+  const [startTime, endTime] = input.split(' - ');
+
+  if (!startTime?.length && !endTime?.length) {
+    return false;
+  }
+
+  const startTimeParsed = startTime.split(':');
+  const endTimeParsed = endTime.split(':');
+
+  if (!startTimeParsed?.length && !endTimeParsed?.length) {
+    return false;
+  }
+
+  const isStartTimeValid = +startTimeParsed[0] < 24 && +startTimeParsed[1] < 60;
+  const isEndTimeValid = +endTimeParsed[0] < 24 && +endTimeParsed[1] < 60;
+
+  return isStartTimeValid && isEndTimeValid;
 };
