d7ac4302809f9e16b88ceff37661cd0f81464296
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-19 19:54:57 +0200

    [Refactor]: Move time checks to separate methods

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,7 +1,7 @@
 import { isMilitaryTime } from './index';
 
 describe('militaryTime validator tests', () => {
-  describe('when incorrect error', () => {
+  describe('when input incorrect', () => {
     describe('when input not correct', () => {
       it('should return false', () => {
         expect(isMilitaryTime('')).toBeFalsy();
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,19 +1,29 @@
+const getTime = (input: string, separator: string): string[] => {
+  return input.split(separator);
+};
+
+const isTimeValid = (time: string[]): boolean => {
+  const [hours, minutes] = time;
+
+  return +hours < 24 && +minutes < 59;
+};
+
 export const isMilitaryTime = (input: string): boolean => {
-  const [startTime, endTime] = input.split(' - ');
+  const [startTime, endTime] = getTime(input, ' - ');
 
   if (!startTime?.length && !endTime?.length) {
     return false;
   }
 
-  const startTimeParsed = startTime.split(':');
-  const endTimeParsed = endTime.split(':');
+  const startTimeParsed = getTime(startTime, ':');
+  const endTimeParsed = getTime(endTime, ':');
 
   if (!startTimeParsed?.length && !endTimeParsed?.length) {
     return false;
   }
 
-  const isStartTimeValid = +startTimeParsed[0] < 24 && +startTimeParsed[1] < 60;
-  const isEndTimeValid = +endTimeParsed[0] < 24 && +endTimeParsed[1] < 60;
+  const isStartTimeValid = isTimeValid(startTimeParsed);
+  const isEndTimeValid = isTimeValid(endTimeParsed);
 
   return isStartTimeValid && isEndTimeValid;
 };
