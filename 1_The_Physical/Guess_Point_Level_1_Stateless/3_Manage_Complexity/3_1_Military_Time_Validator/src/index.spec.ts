ed82ad706109dbdb1cfd3360b68fac319ac2d3b1
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-19 20:00:20 +0200

    [Refactor]: Move error cases to one describe group

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,29 +5,29 @@ describe('militaryTime validator tests', () => {
     it('should return false', () => {
       expect(isMilitaryTime('')).toBeFalsy();
     });
+
+    describe('when time hours is not valid', () => {
+      it('should return false', () => {
+        expect(isMilitaryTime('19:23 - 14:45')).toBeFalsy();
+      });
+    });
+
+    describe('when hours the same and minutes is not valid', () => {
+      it('should return false', () => {
+        expect(isMilitaryTime('14:48 - 14:45')).toBeFalsy();
+      });
+    });
+
+    describe('when input with not proper time length', () => {
+      it('should return false', () => {
+        expect(isMilitaryTime('02029:303002 - 3929:22')).toBeFalsy();
+      });
+    });
   });
 
   describe('when input correct', () => {
     it('should return true', () => {
       expect(isMilitaryTime('04:23 - 14:45')).toBeTruthy();
-    });
-  });
-
-  describe('when time hours is not valid', () => {
-    it('should return false', () => {
-      expect(isMilitaryTime('19:23 - 14:45')).toBeFalsy();
-    });
-  });
-
-  describe('when hours the same and minutes is not valid', () => {
-    it('should return false', () => {
-      expect(isMilitaryTime('14:48 - 14:45')).toBeFalsy();
-    });
-  });
-
-  describe('when input with not proper time length', () => {
-    it('should return false', () => {
-      expect(isMilitaryTime('02029:303002 - 3929:22')).toBeFalsy();
     });
   });
 });
