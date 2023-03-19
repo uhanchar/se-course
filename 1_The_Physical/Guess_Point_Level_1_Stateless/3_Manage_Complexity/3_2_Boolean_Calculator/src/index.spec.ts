35e097e98c36a342c15a5bc58f619f761b9f4a4b
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-19 20:19:27 +0200

    [Refactor]: Move tests with end to one level above in cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -37,32 +37,32 @@ describe('BooleanCalculator tests', () => {
         expect(result).toBeTruthy();
       });
     });
+  });
 
-    describe('when AND provided', () => {
-      describe('when no negation (NOT) provided', () => {
-        describe('when all parts TRUE', () => {
-          it('should return true', () => {
-            const result = sut.calculate('TRUE AND TRUE AND TRUE');
+  describe('when AND provided', () => {
+    describe('when no negation (NOT) provided', () => {
+      describe('when all parts TRUE', () => {
+        it('should return true', () => {
+          const result = sut.calculate('TRUE AND TRUE AND TRUE');
 
-            expect(result).toBeTruthy();
-          });
+          expect(result).toBeTruthy();
         });
+      });
 
-        describe('when not all parts TRUE', () => {
+      describe('when not all parts TRUE', () => {
+        it('should return false', () => {
+          const result = sut.calculate('TRUE AND TRUE AND FALSE');
+
+          expect(result).toBeFalsy();
+        });
+      });
+
+      describe('when negation (NOT) provided', () => {
+        describe('when all parts TRUE with NOT in ONE', () => {
           it('should return false', () => {
-            const result = sut.calculate('TRUE AND TRUE AND FALSE');
+            const result = sut.calculate('TRUE AND TRUE AND NOT TRUE');
 
             expect(result).toBeFalsy();
-          });
-        });
-
-        describe('when negation (NOT) provided', () => {
-          describe('when all parts TRUE with NOT in ONE', () => {
-            it('should return false', () => {
-              const result = sut.calculate('TRUE AND TRUE AND NOT TRUE');
-
-              expect(result).toBeFalsy();
-            });
           });
         });
       });
