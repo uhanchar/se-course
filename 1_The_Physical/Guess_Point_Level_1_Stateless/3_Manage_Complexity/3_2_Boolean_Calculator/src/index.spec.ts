84c1fae896edac3ce9c03b7fd931ff88831a8e41
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-19 20:09:27 +0200

    [New]: Expect calculator handler multiple parts with AND

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -39,5 +39,29 @@ describe('BooleanCalculator tests', () => {
         expect(result).toBeTruthy();
       });
     });
+
+    describe('when AND provided', () => {
+      describe('when no negation (NOT) provided', () => {
+        describe('when all parts TRUE', () => {
+          it('should return true', () => {
+            const sut = new BooleanCalculator();
+
+            const result = sut.calculate('TRUE AND TRUE AND TRUE');
+
+            expect(result).toBeTruthy();
+          });
+        });
+
+        describe('when not all parts TRUE', () => {
+          it('should return false', () => {
+            const sut = new BooleanCalculator();
+
+            const result = sut.calculate('TRUE AND TRUE AND FALSE');
+
+            expect(result).toBeFalsy();
+          });
+        });
+      });
+    });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -8,6 +8,13 @@ export class BooleanCalculator {
       return !(input === 'TRUE');
     }
 
-    return input === 'TRUE';
+    let result = true;
+    const values = input.split(' AND ');
+
+    for (let i = 0; i < values.length; i++) {
+      result = result && values[i] === 'TRUE';
+    }
+
+    return result;
   }
 }
