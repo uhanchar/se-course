88d8d05acaffca1b355638e972cb5831608bf042
Uladzimir Hanchar <uladzimir.hanchar@gmail.com>
2023-03-26 15:22:14 +0300

    [Refactor]: Move time validation into separate method

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -8,6 +8,14 @@ const isTimeCorrect = (time: string[]): boolean => {
   return +hours <= 23 && +minutes <= 59;
 };
 
+const validateTime = (startTime: string[], endTime: string[]): boolean => {
+  const isHoursValid = startTime[0] <= endTime[0];
+  const isMinutesValid =
+    startTime[0] === endTime[0] ? startTime[1] < endTime[1] : true;
+
+  return isHoursValid && isMinutesValid;
+};
+
 export const isMilitaryTime = (input: string): boolean => {
   if (!input) {
     return false;
@@ -29,8 +37,7 @@ export const isMilitaryTime = (input: string): boolean => {
   const isStartTimeCorrect = isTimeCorrect(startTimeParsed);
   const isEndTimeCorrect = isTimeCorrect(endTimeParsed);
   const isHoursCorrect = startTimeParsed[0] <= endTimeParsed[0];
-  const isMinutesValid =
-    startTimeParsed[0] === endTimeParsed[0] ? startTimeParsed[1] < endTimeParsed[1] : true;
+  const isTimeValid = validateTime(startTimeParsed, endTimeParsed);
 
-  return isStartTimeCorrect && isEndTimeCorrect && isHoursCorrect && isMinutesValid;
+  return isStartTimeCorrect && isEndTimeCorrect && isHoursCorrect && isTimeValid;
 };
